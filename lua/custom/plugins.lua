c00cef0b080378871cd12bd0da2c5d3fddde42fb
Mike AUBENAS <mike.aubenas@gmail.com>
2024-03-29 17:59:09 +0100

    fix(git): remove previous plugin in favor of lazygit

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -55,15 +55,6 @@ local plugins = {
     end,
   },
 
-  {
-    "rbong/vim-flog",
-    lazy = true,
-    cmd = { "Flog", "Flogsplit", "Floggit" },
-    dependencies = {
-      "tpope/vim-fugitive",
-    },
-  },
-
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
 
   {
@@ -88,6 +79,26 @@ local plugins = {
         formatting = { format = require("tailwindcss-colorizer-cmp").formatter },
       }
     end,
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
 
   -- To make a plugin not be loaded
