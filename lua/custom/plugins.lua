0ff22f5f93f6fa33f33034b8cc57f35d0d6f3a2e
Mike AUBENAS <mike.aubenas@gmail.com>
2024-05-25 01:12:41 +0200

    feat(org): orgmode

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -178,6 +178,26 @@ local plugins = {
     end,
   },
 
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup {
+        org_agenda_files = "~/orgfiles/**/*",
+        org_default_notes_file = "~/orgfiles/refile.org",
+      }
+
+      -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
+      -- add `org` to ignore_install
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = "all",
+        ignore_install = { "org" },
+      }
+    end,
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
