37cee78e641dc3f96f318bd68a7f1ca7d0778d00
Mike AUBENAS <mike.aubenas@gmail.com>
2024-05-30 13:21:20 +0200

    feat(lang): add python lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,13 +1,13 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local config = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "tailwindcss", "eslint" }
+local servers = { "html", "cssls", "tsserver", "clangd", "tailwindcss", "eslint", "pylint", "pyright", "python-lsp-server" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  config[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
   }
