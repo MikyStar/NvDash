b8940b9d42c49f0efbfc26aa7f93baf78336e8db
Mike AUBENAS <mike.aubenas@gmail.com>
2025-10-29 22:15:25 +0100

    feat: whitespaces + custom commands

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,11 @@
 require "nvchad.options"
 
--- add yours here!
+vim.opt.relativenumber = true
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.listchars = {
+  eol = '↲',
+  trail = '•',
+  space = '•'
+}
+
+vim.opt.list = true
