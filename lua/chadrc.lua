0e453b618c4808f64569a336872ba65b7d980fd8
Mike AUBENAS <mike.aubenas@gmail.com>
2025-10-29 19:20:12 +0100

    feat: dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,12 +1,14 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
+--------------------------------------------------------------------
+
 M.base46 = {
-	theme = "onedark",
+	theme = "radium",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
@@ -14,7 +16,68 @@ M.base46 = {
 	-- },
 }
 
--- M.nvdash = { load_on_startup = true }
+--------------------------------------------------------------------
+
+local handle = io.popen "pwd"
+local pwd = handle:read()
+
+local pwdLine = pwd
+local spaceToFill = (50 - string.len(pwd))
+for i = 0, spaceToFill do
+  if i % 2 == 0 then
+    pwdLine = pwdLine .. " "
+  else
+    pwdLine = " " .. pwdLine
+  end
+end
+
+
+
+M.nvdash = {
+	load_on_startup = true,
+
+
+
+-- " ███╗   ██╗██╗   ██╗██████╗  █████╗ ███████╗██╗  ██╗ "
+-- " ████╗  ██║██║   ██║██╔══██╗██╔══██╗██╔════╝██║  ██║ "
+-- " ██╔██╗ ██║██║   ██║██║  ██║███████║███████╗███████║ "
+-- " ██║╚██╗██║╚██╗ ██╔╝██║  ██║██╔══██║╚════██║██╔══██║ "
+-- " ██║ ╚████║ ╚████╔╝ ██████╔╝██║  ██║███████║██║  ██║ "
+-- " ╚═╝  ╚═══╝  ╚═══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ "
+
+
+
+	header = {
+      "                                                    ",
+      " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                    ",
+      "                                                    ",
+      pwdLine,
+      "                                                    ",
+      "                                                    ",
+    },
+
+	buttons = {
+		{ txt = "󰈚  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+		{ txt = "  Find File", keys = "ff",  cmd ="Telescope find_files" },
+		{ txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+		{ txt = "  Bookmarks", keys = "ma", cmd = "Telescope marks" },
+		{ txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+	},
+}
+
+--------------------------------------------------------------------
+
+M.cheatsheet = {
+	theme = "grid"
+}
+
+
 -- M.ui = {
 --       tabufline = {
 --          lazyload = false
