0cd9dd14d8e28a830489f65f06b312208214eacb
Mike AUBENAS <mike.aubenas@gmail.com>
2025-10-29 20:35:49 +0100

    feat: mappings done

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,25 @@ local map = vim.keymap.set
 --------------------------------------------------------------------
 ----------------------- Normal mode
 
+map("n", "<C-e>", "5<C-e>", { desc = "Scroll down 5 lines" })
+map("n", "<C-y>", "5<C-y>", { desc = "Scroll up 5 lines" })
+
+map("n", "<S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<S-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+
+map("n", "<leader>sv", "<cmd> vsplit <CR>", { desc = "Split vertical" })
+map("n", "<leader>sh", "<cmd> split <CR>", { desc = "Split horizontal" })
+
+map("n", "<leader>ww", "<cmd> lua require('nvim-window').pick()<CR>", { desc = "Pick window" })
+
+map("n", "<C-p>", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", { desc = "Find files" })
+map("n", "<leader>fr", "<cmd> Telescope resume <CR>", { desc = "Resume last Telescope session" })
+
+map("n", "<leader>gn", "<cmd> Gitsigns next_hunk <CR>", { desc = "Next git hunk" })
+map("n", "<leader>gp", "<cmd> Gitsigns prev_hunk <CR>", { desc = "Previous git hunk" })
+map("n", "<leader>gr", "<cmd> Gitsigns reset_hunk <CR>", { desc = "Reset git hunk" })
+map("n", "<leader>gb", "<cmd> Gitsigns blame_line <CR>", { desc = "Blame git hunk" })
+
 map("n", "<Esc>", "<cmd> noh <CR>", { desc = "Clear highlights" })
 
 -- switch between windows
@@ -40,6 +59,33 @@ map("n", "<leader>ch", "<cmd> NvCheatsheet <CR>", { desc = "Mapping cheatsheet" 
 
 map("n", "<leader>lb", function() vim.lsp.buf.format { async = true } end, { desc = "LSP formatting" })
 
+map("n", "<leader>/", function() require("Comment.api").toggle.linewise.current() end, { desc = "Toggle comment" })
+
+-- LSP
+map("n", "gD", function() vim.lsp.buf.declaration() end, { desc = "LSP declaration" })
+map("n", "<leader>D", function() vim.lsp.buf.type_definition() end, { desc = "LSP definition type" })
+map("n", "gd", function() vim.lsp.buf.definition() end, { desc = "LSP definition" })
+map("n", "K", function() vim.lsp.buf.hover() end, { desc = "LSP hover" })
+map("n", "gi", function()  vim.lsp.buf.implementation() end, { desc = "LSP implementation" })
+map("n", "gr", function() vim.lsp.buf.references() end, { desc = "LSP references" })
+map("n", "<leader>ls", function() vim.lsp.buf.signature_help() end, { desc = "LSP signature help" })
+map("n", "<leader>lr", function() require("nvchad.renamer").open() end, { desc = "LSP rename" })
+map("n", "<leader>la", function()  vim.lsp.buf.code_action() end, { desc = "LSP code action" })
+map("n", "<leader>lf", function() vim.diagnostic.open_float { border = "rounded" } end, { desc = "Floating diagnostic" })
+map("n", "<leader>lp", function() vim.diagnostic.goto_prev { float = { border = "rounded" } } end, { desc = "LSP goto previous" })
+map("n", "<leader>ln", function() vim.diagnostic.goto_next { float = { border = "rounded" } } end, { desc = "LSP goto next" })
+map("n", "<leader>q", function() vim.diagnostic.setloclist() end, { desc = "Diagnostic setloclist" })
+
+-- Telescope
+map("n", "<leader>ff", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", { desc = "" })
+map("n", "<leader>fw", "<cmd> Telescope live_grep <CR>", { desc = "Live grep" })
+map("n", "<leader>fb", "<cmd> Telescope buffers <CR>", { desc = "Find buffers" })
+map("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>", { desc = "Find oldfiles" })
+map("n", "<leader>fq", "<cmd> Telescope quickfix <CR>", { desc = "Quickfixes" })
+map("n", "<leader>gc", "<cmd> Telescope git_commits <CR>", { desc = "Git commits" })
+map("n", "<leader>gs", "<cmd> Telescope git_status <CR>", { desc = "Git status" })
+map("n", "<leader>fm", "<cmd> Telescope marks <CR>", { desc = "Bookmarks" })
+
 --------------------------------------------------------------------
 ----------------------- Interactive mode
 
@@ -58,9 +104,16 @@ map("i", "<C-k>", "<Up>", { desc = "Move up" })
 
 map("v", "<Up>", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = "Move up", expr = true })
 map("v", "<Down>", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { desc = "Move down", expr = true })
+
 map("v", "<", "<gv", { desc = "Unindent line" })
 map("v", ">", ">gv", { desc = "Indent line" })
+
 map("v", "<C-z>", "")
+
+map("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+map("v", "<leader>/", function() require("Comment.api").toggle.linewise.current() end, { desc = "Toggle comment" })
 
 --------------------------------------------------------------------
 ----------------------- Other mode
